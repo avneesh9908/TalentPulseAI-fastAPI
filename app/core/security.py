1276c783f1be4197fd31d6fbbee2ccbacdcff6d0
Avneesh kaushik <avikaushik000@gmail.com>
2026-03-09 23:10:19 +0530

    add .env and setup database use hole codebase .env

--- app/core/security.py
+++ app/core/security.py
@@ -7,3 +7,14 @@ def hash_password(password: str):
 
 def verify_password(password: str, hashed_password: str):
     return pwd_context.verify(password, hashed_password)
+
+
+
+    # DATABASE_URL = postgresql://postgres:admin123@localhost:5432/talentPulseAI
+# DATABASE_URL = postgresql://postgres:admin123@localhost:5432/talentpulseAI
+
+
+
+# SECRET_KEY = "mysecretkey123"
+# ALGORITHM = "HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES = 30
